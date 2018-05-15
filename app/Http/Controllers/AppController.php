ce7c551b47c6f5d5414db597a59ba62eb502bd1e
Ikkew-san <ekkarat.no@gmail.com>
2018-05-15 15:40:54 +0700

    get promotion

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\promotion;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class AppController extends BaseController
 {
-    //
+    public function getPromotion() {
+        $results = Promotion::where('end_date', '>=', date("Y-m-d"))->get();
+        return response()->json($results);
+    }
 }
